6e6ca49537c4bf501df9a1b543bc582c64039fce
Conscat <wgooch2000@gmail.com>
2021-10-22 19:42:21 -0400

    Restarted project

--- src/cpp/swapchain.cpp
+++
@@ -1,3 +0,0 @@
-#include "swapchain.hpp"
-
-namespace crow {}
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
-#include <vulkan/vulkan.hpp>
-
-#include <SDL_vulkan.h>
-#include <exception>
 #include <iostream>
-#include <stdexcept>
+#include <vulkan/vulkan.h>
 
-#include "hpp/window.hpp"
+#include "../ext/Vulkan-WSIWindow/WSIWindow/CDevices.h"
+#include "../ext/Vulkan-WSIWindow/WSIWindow/WSIWindow.h"
+
+class MyWindow : public WSIWindow {};
 
 auto main(int /*argc*/, char* /*argv*/[]) -> int {
-    // TODO: Set up fmt::
-    std::cout << "Hello, player!\n";
-    crow::game game;
-    try {
-        game.initialize();
-        game.loop();
-        game.destroy();
-    } catch (std::exception& e) {
-        std::cerr << e.what() << "\n";
-        return EXIT_FAILURE;
-    };
+    std::cout << "Hello, user!\n";
+
+    CInstance instance(true);
+    instance.DebugReport.SetFlags(8);  // Minimal debug information.
+    MyWindow window;
+    window.SetTitle("");
+    window.SetWinSize(640, 480);
+    VkSurfaceKHR p_surface = window.GetSurface(instance);
+
+    CPhysicalDevices gpus(instance);  // Enumerate GPUs and their properties.
+    CPhysicalDevice* p_gpu = gpus.FindPresentable(p_surface);
+
+    CDevice device(*p_gpu);  // Create logical device on selected GPU.
+    CQueue* p_queue = device.AddQueue(VK_QUEUE_COMPUTE_BIT, p_surface);
+
+    // Main event loop, runs until window is closed.
+    while (window.ProcessEvents()) {
+    }
+
     return EXIT_SUCCESS;
 }
