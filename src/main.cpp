976ad1a6987360f51df4d8c85becc90b531f23cc
Conscat <wgooch2000@gmail.com>
2021-10-27 19:33:01 -0400

    Initted new window

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,25 @@
-#include <GLFW/glfw3.h>
 #include <iostream>
 #include <vulkan/vulkan.h>
 
-#include "../ext/Vulkan-WSIWindow/WSIWindow/CDevices.h"
-#include "../ext/Vulkan-WSIWindow/WSIWindow/WSIWindow.h"
+#include "app.hpp"
 
-class MyWindow : public WSIWindow {};
+struct RayTracingScratchBuffer {
+    uint64_t deviceAddress = 0;
+    VkBuffer handle = VK_NULL_HANDLE;
+    VkDeviceMemory memory = VK_NULL_HANDLE;
+};
+
+struct AccelerationStructure {
+    VkAccelerationStructureKHR handle;
+    uint64_t deviceAddress = 0;
+    VkDeviceMemory memory;
+    VkBuffer buffer;
+};
 
 auto main(int /*argc*/, char* /*argv*/[]) -> int {
     std::cout << "Hello, user!\n";
-    CLayers layers;
-    layers.Pick({
-        "VK_LAYER_KHRONOS_validation",
-    });
-    CInstance instance(true);
-    CExtensions extensions;
-    extensions.PickAll();
-
-    MyWindow window;
-    window.SetTitle("");
-    window.SetWinSize(640, 480);
-    VkSurfaceKHR p_surface = window.GetSurface(instance);
-
-    CPhysicalDevices gpus(instance);  // Enumerate GPUs and their properties.
-    CPhysicalDevice* p_gpu = gpus.FindPresentable(p_surface);
-
-    CDevice device(*p_gpu);
-    CQueue* p_queue = device.AddQueue(VK_QUEUE_COMPUTE_BIT, p_surface);
-
-    // layers.Print();
-    // extensions.Print();
-
-    // Main event loop, runs until window is closed.
-    while (window.ProcessEvents()) {
-    }
+    App app;
+    app.initialize();
 
     return EXIT_SUCCESS;
 }
