a7cbe03e59b191bc322ab09057e30bd0ad3cfb35
Conscat <wgooch2000@gmail.com>
2021-10-31 20:11:01 -0400

    Improved create_buffer

--- src/cpp/memory.cpp
+++ src/cpp/memory.cpp
@@ -23,7 +23,6 @@ void create_buffer(VkDevice* p_logical_device,
                    VkBufferUsageFlags usageFlags,
                    VkMemoryPropertyFlags memoryPropertyFlags, VkDeviceSize size,
                    VkBuffer* p_buffer, VkDeviceMemory* p_memory, void* p_data) {
-    // Create the buffer handle
     VkBufferCreateInfo buffer_create_info = {
         .sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
         .size = size,
@@ -31,19 +30,22 @@ void create_buffer(VkDevice* p_logical_device,
         .sharingMode = VK_SHARING_MODE_EXCLUSIVE,
     };
 
-    // TODO: Check result.
-    vkCreateBuffer(*p_logical_device, &buffer_create_info, nullptr, p_buffer);
+    if (vkCreateBuffer(*p_logical_device, &buffer_create_info, nullptr,
+                       p_buffer) != VK_SUCCESS) {
+        stx::panic("Failed to create buffer!");
+    }
 
     // Create the memory backing up the buffer handle
     VkMemoryRequirements mem_reqs;
-    VkMemoryAllocateInfo mem_alloc = vks::initializers::memoryAllocateInfo();
     vkGetBufferMemoryRequirements(*p_logical_device, *p_buffer, &mem_reqs);
-    mem_alloc.allocationSize = mem_reqs.size;
-    // Find a memory type index that fits the properties of the buffer
-    mem_alloc.memoryTypeIndex = find_memory_type(
-        mem_reqs.memoryTypeBits, memoryPropertyFlags, p_physical_device);
-    // If the buffer has VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT set we also
-    // need to enable the appropriate flag during allocation
+
+    VkMemoryAllocateInfo mem_alloc = {
+        .sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_FLAGS_INFO,
+        .allocationSize = mem_reqs.size,
+        .memoryTypeIndex = find_memory_type(
+            mem_reqs.memoryTypeBits, memoryPropertyFlags, p_physical_device),
+    };
+
     VkMemoryAllocateFlagsInfoKHR alloc_flags_info{};
     if (usageFlags & VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT) {
         alloc_flags_info.sType =
@@ -51,31 +53,32 @@ void create_buffer(VkDevice* p_logical_device,
         alloc_flags_info.flags = VK_MEMORY_ALLOCATE_DEVICE_ADDRESS_BIT_KHR;
         mem_alloc.pNext = &alloc_flags_info;
     }
-    // TODO: Check result.
-    vkAllocateMemory(*p_logical_device, &mem_alloc, nullptr, p_memory);
 
-    // If a pointer to the buffer data has been passed, map the buffer and copy
-    // over the data
+    if (vkAllocateMemory(*p_logical_device, &mem_alloc, nullptr, p_memory) !=
+        VK_SUCCESS) {
+        stx::panic("Failed to allocat buffer memory!");
+    }
+
     if (p_data != nullptr) {
         void* p_mapped;
-        // TODO: Check result.
-        vkMapMemory(*p_logical_device, *p_memory, 0, size, 0, &p_mapped);
+        if (vkMapMemory(*p_logical_device, *p_memory, 0, size, 0, &p_mapped) !=
+            VK_SUCCESS) {
+            stx::panic("Failed to map buffer memory!");
+        }
         memcpy(p_mapped, p_data, size);
 
-        // If host coherency hasn't been requested, do a manual flush to make
-        // writes visible
         if ((memoryPropertyFlags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT) == 0) {
-            VkMappedMemoryRange mapped_range =
-                vks::initializers::mappedMemoryRange();
-            mapped_range.memory = *p_memory;
-            mapped_range.offset = 0;
-            mapped_range.size = size;
+            VkMappedMemoryRange mapped_range = {
+                .sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE,
+                .memory = *p_memory,
+                .offset = 0,
+                .size = size,
+            };
             vkFlushMappedMemoryRanges(*p_logical_device, 1, &mapped_range);
         }
         vkUnmapMemory(*p_logical_device, *p_memory);
     }
 
-    // Attach the memory to the buffer object
     // TODO: Check result.
     vkBindBufferMemory(*p_logical_device, *p_buffer, *p_memory, 0);
 }
--- src/hpp/memory.hpp
+++ src/hpp/memory.hpp
@@ -6,7 +6,8 @@
 auto find_memory_type(uint32_t type_filter, VkMemoryPropertyFlags properties,
                       VkPhysicalDevice* p_physical_device) -> uint32_t;
 
-void create_buffer(VkBufferUsageFlags usageFlags,
+void create_buffer(VkDevice* p_logical_device,
+                   VkPhysicalDevice* p_physical_device,
+                   VkBufferUsageFlags usageFlags,
                    VkMemoryPropertyFlags memoryPropertyFlags, VkDeviceSize size,
-                   VkBuffer* p_buffer, VkDeviceMemory* p_memory,
-                   void* p_data = nullptr);
+                   VkBuffer* p_buffer, VkDeviceMemory* p_memory, void* p_data);
